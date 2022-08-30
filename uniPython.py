596cf121294b46546ec441550e41a89a91cb48b4
Rafal Bedkowski <bekowe.fara@gmail.com>
2022-08-30 11:30:42 +0200

    covering basic of the language

--- uniPython.py
+++ uniPython.py
@@ -5,3 +5,20 @@ Created on Tue Aug 30 10:46:55 2022
 @author: Rafał
 """
 
+
+#variables
+
+#String 
+# name = 'Al' v name= "Al"
+
+#int
+#age = 21 whole numbers
+
+#float
+#height = 250.5 whole with decimal points
+
+#height = 250.1
+#print(str(height)) we convert float to string
+
+ #boolean
+ #true or false
